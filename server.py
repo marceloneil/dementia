06560650ef62f98b8d4ebe6d5fbd89a4e7d4aeeb
Marcel O'Neil <marcel@marceloneil.com>
2017-03-04 17:50:17 -0500

    Initial Commit

---
+++ server.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, request, redirect, render_template, url_for
+from werkzeug.utils import secure_filename
+import psycopg2
+
+UPLOAD_FOLDER = os.path.realpath("upload")
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+conn_string = "host='localhost' dbname='dementia' user='postgres'"
+conn = psycopg2.connect(conn_string)
+cur = conn.cursor()
+
+def csv(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() == "csv"
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    print(request)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(url_for("index", error="part"))
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(url_for("index", error="select"))
+        if not csv(file.filename):
+            return redirect(url_for("index", error="csv"))
+        if file and csv(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for("index", success="true"))
+        else:
+            return redirect(url_for("index", error="unknown"))
+    error = request.args.get('error')
+    success = request.args.get('success')
+    return render_template('index.html', error=error, success=success)
+
+@app.route("/post", methods=["POST"])
+def post():
+    json = request.get_json()
+    cur.execute("INSERT INTO dementia_patients VALUES (%s,%s,%s,%s,%s,%s)",
+        (json["EEG1"],json["EEG2"],json["EEG3"],json["EEG4"],json["EEG5"],json["AUX_LEFT"],json["AUX_RIGHT"],"thing"))
+    cur.commit()
+    return "success"
+
+if __name__ == "__main__":
+    app.run()
