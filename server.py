e6797daeef475f58bdcfd005fc79d0370dc5b246
Marcel O'Neil <marcel@marceloneil.com>
2017-03-04 17:57:05 -0500

    Debug

--- server.py
+++ server.py
@@ -39,6 +39,7 @@ def index():
 @app.route("/post", methods=["POST"])
 def post():
     json = request.get_json()
+    print(json["EEG1"],json["EEG2"],json["EEG3"],json["EEG4"],json["EEG5"],json["AUX_LEFT"],json["AUX_RIGHT"])
     cur.execute("INSERT INTO dementia_patients VALUES (%s,%s,%s,%s,%s,%s)",
         (json["EEG1"],json["EEG2"],json["EEG3"],json["EEG4"],json["EEG5"],json["AUX_LEFT"],json["AUX_RIGHT"],"thing"))
     cur.commit()
