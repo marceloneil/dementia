46d37af37ef415b5c46fd40c0160f31459a41bca
Marcel O'Neil <marcel@marceloneil.com>
2017-03-04 22:03:54 -0500

    New endpoint

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
 import psycopg2
 
-UPLOAD_FOLDER = os.path.realpath("upload")
+UPLOAD_FOLDER = os.path.realpath('upload')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -13,35 +13,50 @@ cur = conn.cursor()
 
 def csv(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() == "csv"
+           filename.rsplit('.', 1)[1].lower() == 'csv'
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     print(request)
     if request.method == 'POST':
         if 'file' not in request.files:
-            return redirect(url_for("index", error="part"))
+            return redirect(url_for('index', error='part'))
         file = request.files['file']
         if file.filename == '':
-            return redirect(url_for("index", error="select"))
+            return redirect(url_for('index', error='select'))
         if not csv(file.filename):
-            return redirect(url_for("index", error="csv"))
+            return redirect(url_for('index', error='csv'))
         if file and csv(file.filename):
             filename = secure_filename(file.filename)
+            print(file)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for("index", success="true"))
+            return redirect(url_for('index', success='true'))
         else:
-            return redirect(url_for("index", error="unknown"))
+            return redirect(url_for('index', error='unknown'))
     error = request.args.get('error')
     success = request.args.get('success')
     return render_template('index.html', error=error, success=success)
 
-@app.route("/post", methods=["POST"])
+@app.route('/post', methods=['POST'])
 def post():
     json = request.get_json()
-    cur.execute("INSERT INTO dementia_patients (eeg1, eeg2, eeg3, eeg4, aux1, aux2, objects) VALUES (%s,%s,%s,%s,%s,%s,%s)",
-        (json["EEG1"],json["EEG2"],json["EEG3"],json["EEG4"],json["AUX_LEFT"],json["AUX_RIGHT"],"thing"))
-    return "success"
+    cur.execute('INSERT INTO dementia_patients (eeg1, eeg2, eeg3, eeg4, aux1, aux2, objects) VALUES (%s,%s,%s,%s,%s,%s,%s)',
+        (json['EEG1'],json['EEG2'],json['EEG3'],json['EEG4'],json['AUX_LEFT'],json['AUX_RIGHT'],'thing'))
+    return 'success'
 
-if __name__ == "__main__":
+@app.route('/data')
+def data():
+    if request.method == 'POST':
+        json = request.get_json()
+        cur.execute('INSERT INTO dementia_patients (eeg1, eeg2, eeg3, eeg4, aux1, aux2, objects) VALUES (%s,%s,%s,%s,%s,%s,%s)',
+            (json['EEG1'],json['EEG2'],json['EEG3'],json['EEG4'],json['AUX_LEFT'],json['AUX_RIGHT'],'thing'))
+        return 'success'
+    elif request.method == 'DELETE':
+        cur.execute('TRUNCATE TABLE dementia_patients')
+        return 'success'
+    cur.execute('SELECT * FROM dementia_patients')
+    return cur.fetchall()
+    json = request.get_json()
+
+if __name__ == '__main__':
     app.run()
